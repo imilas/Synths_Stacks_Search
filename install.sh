debb8a9e9b40c47c1179bc2e3a62a79951bb4b39
amir <asalimi@ualberta.ca>
2020-07-02 14:09:26 -0600

    archived some files

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 pippi_source=https://github.com/luvsound/pippi.git
 git clone $pippi_source && cd pippi
 pip install -r requirements.txt
+sudo apt-get install python3-dev
 make install
 cd ..
 pip install -r requirements.txt
